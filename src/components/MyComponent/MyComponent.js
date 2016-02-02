d9f550976f63c62fc6cdb11ff5caadd1358175cc
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-02 13:12:05 +0100

    Initial component

---
+++ src/components/MyComponent/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div styleName="container">
+        <div styleName="rectangle">Hello World!</div>
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
---
+++ src/components/MyComponent/index.js
@@ -0,0 +1,6 @@
+import CSSModules from 'react-css-modules'
+
+import MyComponent from './MyComponent';
+import styles from './styles.less';
+
+export default CSSModules(MyComponent, styles);
