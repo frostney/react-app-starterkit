76e57acd515382588584dbfdf91b66f764c07ffc
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 12:40:17 +0100

    Now with some example image

--- src/components/MyComponent/MyComponent.js
+++ src/components/MyComponent/MyComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { tower } from 'images';
 
 class MyComponent extends Component {
   render() {
     return (
       <div styleName="container">
         <div styleName="rectangle">Hello World!</div>
+        <img src={tower} width={128} height={128} />
       </div>
     );
   }
