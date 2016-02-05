3dbe45e0f706210808f694fcf16509ec777a2805
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 10:00:18 +0100

    Add About screen

---
+++ src/screens/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div styleName="container">
+        <div>About page</div>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/screens/About/index.js
@@ -0,0 +1,6 @@
+import CSSModules from 'react-css-modules'
+
+import About from './About';
+import styles from './styles.css';
+
+export default CSSModules(About, styles);
