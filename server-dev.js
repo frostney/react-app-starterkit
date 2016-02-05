7c78482cee86bf948c029a887fe528d036e16e7e
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 11:34:44 +0100

    More verbosity

--- server-dev.js
+++ server-dev.js
@@ -3,11 +3,9 @@ import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.config.babel';
 import open from 'open';
 
-console.log(config);
-console.log(config.devServer);
+const webpackServer = new WebpackDevServer(webpack(config), config.devServer);
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
+webpackServer.listen(config.port, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
