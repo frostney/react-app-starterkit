fcd47be8670e6074cb34e4fcbd8ba5abd416186f
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 11:19:19 +0100

    Add About screen

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -1,6 +1,6 @@
 import CSSModules from 'react-css-modules'
 
 import About from './About';
-import styles from './styles.css';
+import styles from './styles.less';
 
 export default CSSModules(About, styles);
