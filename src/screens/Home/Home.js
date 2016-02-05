e9415cd61ee10526c60c65a9bd9cf14e085a54bf
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 10:00:29 +0100

    Add Home screen

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { MyComponent } from 'components';
+
+class Home extends Component {
+  render() {
+    return (
+      <div styleName="container">
+        <MyComponent />
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,6 @@
+import CSSModules from 'react-css-modules'
+
+import Home from './Home';
+import styles from './styles.css';
+
+export default CSSModules(Home, styles);
