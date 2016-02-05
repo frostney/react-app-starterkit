6db3cd94befee446811e32ab1cbe02d7ed2eba92
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-05 09:59:57 +0100

    Use hash history

--- src/base/main.js
+++ src/base/main.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { Router } from 'react-router';
 
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createHashHistory from 'history/lib/createHashHistory';
 import routes from './routes';
 
 import 'styles/main.less';
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 
 const content = document.getElementById('content');
 
