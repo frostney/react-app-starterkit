b319dca6e3e53cd76d13a775edcc50ac45ef842c
Johannes Stein <johannesstein@freeze-dev.com>
2016-03-15 19:33:46 +0100

    Use history directly from `react-router`

--- src/base/main.js
+++ src/base/main.js
@@ -4,15 +4,12 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
-import { createHashHistory } from 'history';
 import routes from './routes';
 
 import 'styles/main.less';
 
-const history = createHashHistory();
-
 const content = document.getElementById('content');
 
-ReactDOM.render(<Router history={history}>{routes}</Router>, content);
+ReactDOM.render(<Router history={hashHistory}>{routes}</Router>, content);
