d5b556e0d11351375409ffafaf5896f9deb90a1d
Johannes Stein <johannesstein@freeze-dev.com>
2016-07-27 00:28:45 +0100

    Fixes chunk creation for CSS modules

--- webpack/extracts.js
+++ webpack/extracts.js
@@ -1,6 +1,10 @@
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-const extractCSSModules = new ExtractTextPlugin('style-modules.css');
-const extractCSS = new ExtractTextPlugin('style-global.css');
+const extractCSSModules = new ExtractTextPlugin('style-modules.css', {
+  allChunks: true,
+});
+const extractCSS = new ExtractTextPlugin('style-global.css', {
+  allChunks: true,
+});
 
 export { extractCSSModules, extractCSS };
